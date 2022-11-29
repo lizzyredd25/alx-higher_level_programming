1e6bafd5e21b01e90b87c785736bb321aacbd826
lizzyredd25 <oluwaseunoladele25@gmail.com>
2022-11-28 20:30:18 -0800

    linked

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -5,7 +5,9 @@
 
 /**
  * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct listint_s
